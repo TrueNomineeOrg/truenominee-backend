62e0cb19cd982eed66f7640c361e2ece26778a90
Diwakar <diwakar03121997@gmail.com>
2024-02-17 03:11:01 +0530

    added constants

---
+++ src/models/constants/AssetClass.js
@@ -0,0 +1,11 @@
+const AssetClass = {
+    BANK_DEPOSITS: 'Bank Deposits',
+    MUTUAL_FUNDS: 'Mutual Funds',
+    EQUITY_MARKETS: 'National and International Stock Markets',
+    INSURANCE: 'Insurance Policies',
+    RETIREMENT_ACCOUNTS: 'Retirement Accounts',
+    ESOPS: 'Employer Stock Options',
+    REAL_ESTATE: 'Real Estate Investments',
+    COMMODITIES: 'Commodities',
+    CRYPTOCURRENCY: 'Crypto Currency'
+  }
